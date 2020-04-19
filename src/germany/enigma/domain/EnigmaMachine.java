88aeb60ce7bfbf89c5e0c7beb40e59d0f1a6b825
johnny willer <johnnywiller10@gmail.com>
2020-04-19 18:41:02 +0100

    included reflector to be able to decrypt

--- src/germany/enigma/domain/EnigmaMachine.java
+++ src/germany/enigma/domain/EnigmaMachine.java
@@ -2,18 +2,20 @@ package germany.enigma.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class EnigmaMachine implements EnigmaMachineSubject {
 
     private List<Rotor> rotors;
     private List<KeyPressObserver> keyPressObservers;
     private List<RotorObserver> rotorObservers;
-
+    private Reflector reflector;
 
     public void setup() {
         KeyboardRotorAware firstRotor = new KeyboardRotorAware(new char[]{'Z'}, 'A', null, this);
         Rotor secondRotor = new Rotor(new char[]{'Z'}, 'A', firstRotor, this);
         Rotor thirdRotor = new Rotor(new char[]{'Z'}, 'A', secondRotor, this);
+        reflector = new Reflector();
 
         rotors = new ArrayList<>();
         keyPressObservers = new ArrayList<>();
@@ -34,9 +36,22 @@ public class EnigmaMachine implements EnigmaMachineSubject {
         for (char c : message.toCharArray()) {
             dispatchKeyPressEvents(c);
             char transformed = c;
+
+            // Encrypts forwards
             for (Rotor rotor : rotors) {
                 transformed = rotor.transformCharacter(transformed);
             }
+
+            // Puts into Reflector
+            transformed = reflector.reflect(transformed);
+
+            // Encrypts backwards
+            ListIterator<Rotor> rotorListIterator = rotors.listIterator(rotors.size());
+            while (rotorListIterator.hasPrevious()) {
+                Rotor rotor = rotorListIterator.previous();
+                transformed = rotor.reverseCharacter(transformed);
+            }
+
             cipheredMessage.append(transformed);
         }
         return cipheredMessage.toString();
